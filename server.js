b0462eb727b62393bca659b36c32943e0439ea17
Banx87 <joni.lindholm@gmail.com>
2019-03-04 16:53:55 +0200

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
+        host : 'postgresql-horizontal-13978',
         port: '5433',
         user : 'postgres',
         password : 'Naminam87',
