a3163ea33dbded00eb6a9e7c713827680242d77c
Banx87 <joni.lindholm@gmail.com>
2019-03-05 11:29:01 +0200

    fixing

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const db = knex({
     acquireConnectionTimeout: 5000
 });
 
-var allowCrossDomain = function(req, res, next) {
+/*var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -37,7 +37,7 @@ var allowCrossDomain = function(req, res, next) {
     else {
         next();
     }
-};
+};*/
 
 /*db.select('*').from ('users')
     .then(data => {
@@ -46,8 +46,8 @@ var allowCrossDomain = function(req, res, next) {
 
 const app = express();
 
-app.use(cors());
-app.use(allowCrossDomain);
+//app.use(cors());
+//app.use(allowCrossDomain);
 app.use(bodyParser.json());
 
 //app.options('*', cors());
