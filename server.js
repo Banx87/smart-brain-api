fdeceb3d327c22f81a9c18c8a5e75def21546fb8
Banx87 <joni.lindholm@gmail.com>
2019-03-05 12:19:24 +0200

    Last try

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import * as headers from "express";
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
@@ -64,6 +66,7 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
+headers.append('GET', 'POST', OPTIONS);
 
 //const DATABASE_URL = process.env.DATABASE_URL;
 app.listen(process.env.PORT || 3000, () => {
